5d3c2c222e4d406d77897c9e25ddbf902d17d4dd
jorgen <jorgen.jackwitz@gmail.com>
2021-11-30 23:53:27 +0100

    First comit of aoc2021

---
+++ src/day1.rs
@@ -0,0 +1,12 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+pub fn input_day1(input: &str) -> Vec<u32> {
+    let v = vec![1u32,2,3];
+    v
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &[u32]) -> u32 {
+    0
+}
