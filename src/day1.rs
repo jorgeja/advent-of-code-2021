df12065b42f00c0fc7f0a596b7888d393d00530c
jorgen <jorgen.jackwitz@gmail.com>
2021-12-01 07:23:56 +0100

    Finished day1

--- src/day1.rs
+++ src/day1.rs
@@ -2,11 +2,38 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
 pub fn input_day1(input: &str) -> Vec<u32> {
-    let v = vec![1u32,2,3];
+    let v = input.lines().filter_map(|s| s.parse::<u32>().ok()).collect();
     v
 }
 
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &[u32]) -> u32 {
-    0
+    let mut last = None;
+    let mut increases = 0;
+    for i in input.iter() {
+        if let Some(last_i) = last {
+            if last_i < i {
+                increases += 1;
+            }
+        }
+        last = Some(i);
+    }
+    increases
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[u32]) -> u32 {
+    let mut last = None;
+    let mut increases = 0;
+    for window in input.windows(3) {
+        let sum = window[0] + window[1] + window[2];
+        if let Some(last_i) = last {            
+            if last_i < sum {
+                increases += 1;
+            }
+        }
+
+        last = Some(sum);
+    }
+    increases
 }
